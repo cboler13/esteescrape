1923e67f843225e65a1203512b07be3de3a00bdd
cboler13 <cboler13@users.noreply.github.com>
2024-06-13 16:56:16 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="cboler13"
+original_name="esteescrape"
+original_urlname="esteescrape"
+original_description="Awesome esteescrape created by cboler13"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv esteescrape $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- esteescrape/cli.py
+++ esteescrape/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for esteescrape project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m esteescrape` and `$ esteescrape `.
 
     This is your program's entry point.
 
